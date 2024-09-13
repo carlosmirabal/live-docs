13b157543dca13024c3febb492710a8a8548f1db
Carlos <carlos@ossistemes.com>
2024-09-13 13:55:01 +0200

    first commit

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: true,
+  printWidth: 180,
+  quoteProps: "preserve",
+  endOfLine: "lf",
+  arrowParens: "always",
+  proseWrap: "preserve",
+};
