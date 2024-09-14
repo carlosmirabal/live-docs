0d0d71c8a40ff1128bda147afad5437d57901a49
Carlos <carlos@ossistemes.com>
2024-09-14 15:42:42 +0200

    changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+    </main>
   );
 };
 
