fd2587143b7e48d5ba7f24e9f89984dd70b7455c
Carlos <carlos@ossistemes.com>
2024-09-17 17:07:20 +0200

    edit document title

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getDocument } from "@/lib/actions/room.actions";
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom />
+      <CollaborativeRoom roomId={room.id} roomMetadata={room.metadata} />
     </main>
   );
 };
